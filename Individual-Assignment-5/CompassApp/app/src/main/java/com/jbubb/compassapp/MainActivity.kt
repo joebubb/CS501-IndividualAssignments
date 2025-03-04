d7893342561f00928b2ac59524d7036369cb09ec
joebubb <jbubb@bu.edu>
2025-03-04 01:15:44 -0500

    finished Q2

--- Individual-Assignment-5/CompassApp/app/src/main/java/com/jbubb/compassapp/MainActivity.kt
+++ Individual-Assignment-5/CompassApp/app/src/main/java/com/jbubb/compassapp/MainActivity.kt
@@ -8,13 +8,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.jbubb.compassapp.ui.theme.CompassAppTheme
 
 class MainActivity : ComponentActivity(), SensorEventListener {
@@ -24,6 +31,17 @@ class MainActivity : ComponentActivity(), SensorEventListener {
     private var magnetometer: Sensor? = null
     private var accelerometer: Sensor? = null
     private var gyroscope: Sensor? = null
+
+    // vectors to store data for math
+    private var magneticFieldVector by mutableStateOf(listOf(0f, 0f, 0f))
+    private var accelerometerVector by mutableStateOf(listOf(0f, 0f, 0f))
+
+    // for the level
+    private var bubbleX by mutableFloatStateOf(0f)
+    private var bubbleY by mutableFloatStateOf(0f)
+
+    // switchable mode
+    private var mode by mutableStateOf("level")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,7 +56,19 @@ class MainActivity : ComponentActivity(), SensorEventListener {
         enableEdgeToEdge()
         setContent {
             CompassAppTheme {
+                // render the view for the mode
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    ModeToggleButton(mode) { mode = it }
 
+                    if (mode == "compass") {
+                        CompassApp(calculateAzimuth(accelerometerVector, magneticFieldVector))
+                    } else {
+                        LevelApp(bubbleX, bubbleY)
+                    }
+                }
             }
         }
     }
@@ -64,17 +94,126 @@ class MainActivity : ComponentActivity(), SensorEventListener {
 
     override fun onSensorChanged(event: SensorEvent?) {
         event?.let {
-            when (event.sensor) {
-                magnetometer -> {
-
+            when (event.sensor.type) {
+                Sensor.TYPE_MAGNETIC_FIELD -> {
+                    // set magnetic vector
+                    magneticFieldVector = listOf(it.values[0], it.values[1], it.values[2])
                 }
-                accelerometer -> {
+                Sensor.TYPE_ACCELEROMETER -> {
+                    // set accelerometer vector variable
+                    accelerometerVector = listOf(it.values[0], it.values[1], it.values[2])
 
-                }
-                gyroscope -> {
+                    // get the accelerometer values needed for the level
+                    val x = it.values[0]
+                    val y = it.values[1]
 
+                    bubbleX = -x * 10f
+                    bubbleY = y * 10f
                 }
             }
         }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        // not worrying about accuracy
+    }
+
+    private fun calculateAzimuth(accVector: List<Float>, magVector: List<Float>): Float {
+        // convert to float lists
+        val accelerometerArray = floatArrayOf(
+            accVector[0],
+            accVector[1],
+            accVector[2]
+        )
+        val magnetometerArray = floatArrayOf(
+            magVector[0],
+            magVector[1],
+            magVector[2]
+        )
+
+        // calculate rotation matrix
+        val rotationMatrix = FloatArray(9)
+        val success = SensorManager.getRotationMatrix(
+            rotationMatrix,
+            null,
+            accelerometerArray,
+            magnetometerArray
+        )
+
+        // if successful calculate orientation
+        return if (success) {
+            val orientationAngles = FloatArray(3)
+            SensorManager.getOrientation(rotationMatrix, orientationAngles)
+
+            // azimuth
+            val azimuthInDegrees = Math.toDegrees(orientationAngles[0].toDouble()).toFloat()
+            (azimuthInDegrees + 360) % 360
+        } else {
+            // usually won't fail
+            0f
+        }
+    }
+}
+
+@Composable
+fun CompassApp(deg: Float) {
+    Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(R.drawable.compass_needle),
+            contentDescription = "compass needle",
+            modifier = Modifier.rotate(-deg) // rotate the needle by the calculated azimuth
+        )
+    }
+}
+
+@Composable
+fun LevelApp(xTilt: Float, yTilt: Float) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .size(200.dp)
+                .border(2.dp, Color.Black, CircleShape)
+        ) {
+            //cross hairs
+            Box(
+                modifier = Modifier
+                    .size(width = 2.dp, height = 200.dp)
+                    .background(Color.Black)
+            )
+            Box(
+                modifier = Modifier
+                    .size(width = 200.dp, height = 2.dp)
+                    .background(Color.Black)
+            )
+
+            // limit the bubble movement
+            val maxOffset = 80f
+            val limitedX = xTilt.coerceIn(-maxOffset, maxOffset)
+            val limitedY = yTilt.coerceIn(-maxOffset, maxOffset)
+
+            Box(
+                modifier = Modifier
+                    .size(20.dp)
+                    .offset(x = limitedX.dp, y = limitedY.dp)
+                    .background(Color.Blue, CircleShape)
+            )
+        }
+    }
+}
+
+@Composable
+fun ModeToggleButton(mode: String, onModeChange: (String) -> Unit) {
+    // switches modes
+    Button(
+        onClick = {
+            onModeChange(if (mode == "compass") "level" else "compass")
+        },
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Text(text = if (mode == "compass") "Switch to Level" else "Switch to Compass")
     }
 }
